5092b815787c7895e323eb06dec968b622eff049
bibekshhh <bibekrock2019@gmail.com>
2024-03-16 21:26:34 +0545

    folder re-setup

---
+++ Daily Question/Easy/Product_of_Array_Except _Self.js
@@ -0,0 +1,20 @@
+// Leetcode: https://leetcode.com/problems/product-of-array-except-self/description/?envType=study-plan-v2&envId=leetcode-75
+// My submission:
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+var productExceptSelf = function (nums) {
+  let x = 0;
+};
+
+const test_cases = [
+  productExceptSelf([1, 2, 3, 4]),
+  productExceptSelf([-1, 1, 0, -3, 3]),
+];
+
+test_cases.forEach((test_case) => {
+  console.log(test_case);
+});
